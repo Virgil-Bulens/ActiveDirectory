1a97805feb24637ad00601aa4bd6ef149879c5e6
Virgil Bulens <github@anonymousdaddy.anonaddy.com>
2022-07-24 17:29:42 +0200

    Create Get-LoggedOnComputers.ps1

---
+++ functions/Get-LoggedOnComputers.ps1
@@ -0,0 +1,52 @@
+Function Get-LoggedOnComputers
+{
+    #
+    # Parameters
+    #
+    Param(
+        # Username
+        [Parameter(
+            Mandatory = $true,
+            Position = 0
+        )]
+        [string]
+        $Username,
+
+        # Search Base
+        [Parameter(
+            Mandatory = $false,
+            Position = 1
+        )]
+        [string]
+        $SearchBase = (Get-ADDomain | ForEach-Object DistinguishedName)
+    )
+
+
+    #
+    # Variables
+    #
+    $ErrorActionPreference = "SilentlyContinue"
+    $LoggedOnComputers = @()
+
+
+    #
+    # Main
+    #
+    # Get all domain computers
+    $DomainComputers = Get-ADComputer -Filter { Enabled -eq $true } -SearchBase $SearchBase
+
+    # Query all computers if user is logged on
+    foreach ( $Computer in $DomainComputers )
+    {
+        $Result = query user /server:$($Computer.DNSHostName)
+
+        if ( $Result -contains "*$Username*" )
+        {
+            $LoggedOnComputers += $Computer.Name
+            Write-Output "Found $($Computer.Name)"
+        }
+    }
+
+    # Output the result
+    $LoggedOnComputers
+} 
